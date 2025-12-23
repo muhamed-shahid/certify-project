d4416074b14e0ba0a0ac6a14223bd71bfa12a62a
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-12-23 14:28:31 +0530

    Company Approval page completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminLogin from "./pages/admin/AdminLogin";
 import UniversityApproval from "./pages/admin/UniversityApproval";
+import CompanyApproval from "./pages/admin/CompanyApproval";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Route path="/admin/login" element={<AdminLogin/>}/>
     <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
     <Route path="/admin/univerities" element={<UniversityApproval/>}/>
+    <Route path="/admin/companies" element={<CompanyApproval/>}/>
    </Routes>
    </BrowserRouter>
    
