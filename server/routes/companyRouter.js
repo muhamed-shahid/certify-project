031de5e45a2d7296eb31ac25c55b3e3493bfa2ea
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-02-16 11:25:23 +0530

    Setting company Register backend

--- server/routes/companyRouter.js
+++ server/routes/companyRouter.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const {loginCompany} = require("../controllers/companyController")
+const {loginCompany,registerCompany} = require("../controllers/companyController")
 
 router.post("/login",loginCompany)
+router.post("/register",registerCompany)
 
 module.exports = router
