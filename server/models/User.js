0d168624835f75acad6b84343cbf8afcbe3014cd
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-02-20 17:46:59 +0530

    Setting one authentication  model

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const UserSchema =new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+    },
+
+    password:{
+        type:String,
+        required:true,
+    },
+
+
+    role:{
+        type:String,
+        enum:["ADMIN","UNIVERSITY","COMPANY"],
+        required:true,
+    },
+
+
+    status:{
+        type:String,
+        enum:["PENDING","APPROVED"],
+        default:"PENDING",
+    }
+},
+{timestamps:true}
+)
+
+module.exports = mongoose.model("User",UserSchema)
