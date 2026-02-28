584b12065b62c7e5cd6796e85165640fe5ad1e27
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-02-28 12:42:25 +0530

    connectng admin dashboard backend with frontend

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import AdminSideBar from '../../components/AdminSideBar'
 import AdminHeader from '../../components/AdminHeader'
+import axios from "axios"
+import toast from "react-hot-toast"
 
 const AdminDashboard = () => {
 
-  const [stats,getstats] = useState({
+  const [stats,setStats] = useState({
     universities:0,
     companies:0,
     certificates:0,
@@ -14,8 +16,21 @@ const AdminDashboard = () => {
 
 
   useEffect (()=>{
-    
-  })
+    const fetchstats = async ()=>{
+      try{
+        const res = await axios.get("http://localhost:5055/api/admin/dashboard",{
+          headers:{
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        setStats(res.data.data)
+      }catch(err){
+        toast.error(err.response?.data?.message||"Failed to load dashboard stats")
+      }
+    }
+    fetchstats()
+  },[token])
   return (
     <div className='flex'>
       <AdminSideBar/>
@@ -26,17 +41,17 @@ const AdminDashboard = () => {
         <div className="p-6 grid-cols-1 md:grid-cols-3 gap-6">
           <div className="bg-white rounded-xl shadow">
             <h3 className="text-slate-600">Universities</h3>
-            <p className="text-3xl font-bold">12</p>
+            <p className="text-3xl font-bold">{stats.universities}</p>
 
             </div>
           <div className="bg-white rounded-xl shadow mt-5">
             <h3 className="text-slate-600">Companies</h3>
-            <p className="text-3xl font-bold">8</p>
+            <p className="text-3xl font-bold">{stats.companies}</p>
 
             </div>
           <div className="bg-white rounded-xl shadow mt-5">
             <h3 className="text-slate-600">Certificates</h3>
-            <p className="text-3xl font-bold">125</p>
+            <p className="text-3xl font-bold">{stats.certificates}</p>
 
             </div>
             </div>
